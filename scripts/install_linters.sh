6787bfb983f448b9dc12f0ceee28790254488dbf
Ubuntu <viktor.barinov@woven-planet.global>
2022-08-02 21:55:00 +0000

    try-fix-lint

--- scripts/install_linters.sh
+++ scripts/install_linters.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set -e
+
 # Install Bazel linter.
 wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
 /usr/local/go/bin/go get github.com/bazelbuild/buildtools/buildifier
+sudo rm /usr/bin/buildifier
 sudo ln -s $HOME/go/bin/buildifier /usr/bin/buildifier
 
 # Install C++ linter.
-sudo apt update && sudo apt install clang-format -y
+sudo apt update && sudo apt install clang-format-6.0 -y
 
 # Install Python linter.
 sudo apt-get install python-pip -y
 pip install --upgrade autopep8
+sudo rm /usr/bin/autopep8
 sudo ln -s $HOME/.local/bin/autopep8 /usr/bin/autopep8
 
