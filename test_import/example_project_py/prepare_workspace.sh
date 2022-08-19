4c69d7b22f5781d1bd9f90320ccccf8d8980b20f
Ubuntu <viktor.barinov@woven-planet.global>
2022-08-19 18:56:03 +0000

    fix-py-impirt

--- test_import/example_project_py/prepare_workspace.sh
+++ test_import/example_project_py/prepare_workspace.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-PACKAGE_PATH=tmp\\/slog
-if [[ "${BUILDKITE_BRANCH}" = "main" ]]; then
-    PACKAGE_PATH=slog
-fi
-
 cat WORKSPACE.template \
-    | sed "s/{{version}}/v$1/g" \
-    | sed "s/{{package_path}}/$PACKAGE_PATH/g" \
+    | sed "s/{{git_commit}}/$1/g" \
     > WORKSPACE
+
