7561c59da3b9be0e4f5741c7c953665a41876dfe
Viktor Barinov <victorbarinov@gmail.com>
2023-03-01 14:08:37 -0800

    Update slog_trace_subscriber.h

--- slog_cc/analysis_tools/tracing/slog_trace_subscriber.h
+++ slog_cc/analysis_tools/tracing/slog_trace_subscriber.h
@@ -1,3 +1,20 @@
+// Copyright 2023 Woven Planet Holdings
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef slog_cc_analysis_tools_tracing_slog_trace_subscriber
+#define slog_cc_analysis_tools_tracing_slog_trace_subscriber
+
 #include <cstring>
 #include <fstream>
 #include <map>
@@ -69,3 +86,5 @@ SlogTraceSubscriber CreateSlogTraceSubscriber(
 }
 
 }  // namespace slog
+
+#endif
